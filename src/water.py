2b56e3a8d3fc18b6569b154188a4a25a1145344b
septumfunk <jfleischman6@gmail.com>
2026-05-13 13:16:05 -0700

    District loop and printing

--- src/water.py
+++ src/water.py
@@ -8,8 +8,12 @@ MIN_GALLONS_DELIVERED = 0
 MIN_PEOPLE = 1
 MIN_GALLONS_REQUESTED = 0
 
+DISTRICT_CLOVER_PARK = "*** District 1 (Clover Park) ***"
+DISTRICT_STEILACOOM = "*** District 2 (Steilacoom) ***"
 
 # TODO: Process water distribution data for both districts.
+for district in range(DISTRICT_COUNT):
+    print(DISTRICT_CLOVER_PARK if district == 0 else DISTRICT_STEILACOOM)
 
 # TODO: For each district, process all shelters.
 
